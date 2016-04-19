67c6061ecef287d9047de11f9837d706529b722b
lordgiovanne <lord.giovanne@gmail.com>
2016-04-19 17:51:41 -0300

    Update and rename 01-Imprimir to 01-Comentarios&Impressao.py

---
+++ 01-Comentarios&Impressao.py
@@ -0,0 +1,26 @@
+#   Programas de computador sao codigos fonte escritos em alguma linguagem de programacao
+#convertidos pelo compilador ou interpretador (de acordo com a linguagem em utilizacao)
+#em arquivos em linguagem de maquina e que, por sua vez, utilizam dos circuitos da maquina
+#e sua logica de operacoes para gerar os resultados esperados pelo programador.
+#   Para o programador (interessado apenas no programa pronto) e, consequentemente, para o
+#leitor, so interessa o codigo fonte e suas implicacoes. Dessa forma, iremos trabalhar em
+#cima de como criar ou compreender codigos na linguagem de programacao python.
+#   Para a melhor compreensao dos codigos, e habitual a utilizacao de comentarios ou doc-
+#strings. Estes sao fragmentos do codigo que nao sao lidos pelo interpretador (no caso do
+#python) no caso dos comentarios. Ou sao lidos, mas nao interferem na execucao do programa
+#no caso dos docstrings.
+#   Para inserir comentarios ou docstrings sao usados simbolos reservados na linguagem para
+#tal ato. Como voce ja deve ter percebido, em python o "#" e o caractere responsavel pela
+#quebra do codigo para comentario. Docstrings nao serao necessarios no inicio, portanto nao
+#me estenderei sobre estes.
+
+#   Uma das principais e mais importantes "funcoes" (no caso do python 2 nao e considerada 
+#funcao no sentido logico de programacao da palavra) de qualquer linguagem e a impressao.
+#Esta serve para mostrar na tela o que o programador desejar.
+#   Em python, a impressao e muito simples. Para nos, que estamos comecando, vamos imprimir
+#apenas strings (conjunto de caracteres). Basta seguir o modelo:
+  print "Conteudo a ser impresso."
+#   E importante observar que e incluido automaticamente o caractere fim-de-linha ao usar
+#o print.
+
+#Proxima licao: variaveis.
