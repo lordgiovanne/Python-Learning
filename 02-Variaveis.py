2d18b69d1d5251c82a36980b2402dd15fb7f1ca0
lordgiovanne <lord.giovanne@gmail.com>
2016-04-27 18:00:09 -0300

    Update and rename 02-Variaveis&Aritimetica.py to 02-Variaveis.py
    Fiz o tutorial para tipos de variaveis e decidi separar de operadores

---
+++ 02-Variaveis.py
@@ -0,0 +1,23 @@
+#   Nesta parte do turorial iremos trabalhar com variaveis do tipo numero ou string. Em python, nao e necessario declarar variaveis, 
+#visto que e uma linguagem orientada a objetos (discussao para outro momento).
+
+#   Numeros serao classificados em dois tipos: inteiros ou fracionarios. Basta, portanto, atribuir valores as variaveis (que podem 
+#receber qualquer nome nao reservado a linguagem e contendo os caracteres permitidos) e utiliza-las. Por ex:
+
+a=5    #acabamos de atribuir o tipo inteiro para a variavel "a" e o valor 5
+b=5.0  #acabamos de atribuir o tipo fracionario para a variavel "b" e o valor 5.0
+c=float(7)  #tambem podemos atribuir o tipo fracionario dessa maneira!  
+
+#   Strings sao conjuntos de caracteres e para atribuir o tipo string a uma variavel basta escrever o string desejado entre aspas
+#duplas ou simples. No caso de atribuir com aspas simples, tome cuidado para nao utilizar apostrofes dentro da string. Ou, por
+# padrão, atribua strings sempre usando aspas duplas e evite qualquer problema. Por ex:
+
+d='nao posso usar apostrofes'
+e="nao ' preciso ' mais ' me ' preocupar ' com ' apostrofes ' mal ' interpretados"
+
+#   E possivel atribuir valores a mais de uma variavel ao mesmo tempo. Basta atribuir as com mesmo tipo na mesma linha e seus valores 
+#devem estar de acordo, respectivamente, com suas chamadas. Por ex:
+
+inteiro1, inteiro2 = 1,2
+string1, string2 = "olar", "tchau"
+fracionario1, fracionario2 = float(1), 2.0
