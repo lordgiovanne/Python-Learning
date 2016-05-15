901cf7e67d83aca30401651b8efcb10bd63887a5
lordgiovanne <lord.giovanne@gmail.com>
2016-05-15 13:31:52 -0300

    Create 06-Operar_Strings.py
    Criação do tutorial para ser atualizado posteriormente.

---
+++ 06-Operar_Strings.py
@@ -0,0 +1,11 @@
+astring = "Hello World!"
+print len(astring)
+print astring.index("o")
+print astring.count("l")
+print astring[0:4]
+print astring[::-1]
+print astring.upper()
+print astring.lower()
+separar = astring.split(" ")
+print "%s" % separar
+#comentar e explicar
