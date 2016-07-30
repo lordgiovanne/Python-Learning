35eb463c0ede963d6f98bb09fb369d3f5542fc8e
lordgiovanne <lord.giovanne@gmail.com>
2016-07-30 01:01:57 -0300

    Update 08-Loops.py
    Adicionada explicacao inicial geral

--- 08-Loops.py
+++ 08-Loops.py
@@ -1,1 +1,4 @@
-#
+#   Muitas vezes voce ira se deparar com situacoes em que uma determinada acao deve aparecer repetidas vezes em seu codigo.
+#Para evitar que voce tenha que escrever todas as vezes a mesma sentenca e para criar possibilidades de determinar o numero
+#de vezes que esta sentenca e executada dinamicamente, existem os lacos de loop. Em python os lacos de loop sao o for e o 
+#while.
